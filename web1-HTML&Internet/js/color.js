c5c0c8144ecc48caa683b9d765b4a7b8e8d3890d
hyunseok kim <hskim@cosmotier.com>
2023-06-25 14:58:16 +0900

    jquery 적용

--- web1-HTML&Internet/js/color.js
+++ web1-HTML&Internet/js/color.js
@@ -1,36 +1,39 @@
 var Links = {
     SetColor: function LinksSetColor(color){
-        var alist = document.querySelectorAll('a');
-        var i = 0;
-        while(i < alist.length){
-            console.log(alist[i]);
-            alist[i].style.color = color;
-            i = i + 1;
-        }
+        // var alist = document.querySelectorAll('a');
+        // var i = 0;
+        // while(i < alist.length){
+        //     console.log(alist[i]);
+        //     alist[i].style.color = color;
+        //     i = i + 1;
+        // }
+        $('a').css('color', color);
     }
 }
 var Body = {
     SetColor: function BodySetColor(color){
-        document.querySelector('body').style.color = color;
+        // document.querySelector('body').style.color = color;
+        $('body').css('color', color);
     },
-
+    
     SetBackgourndColor: function BodySetBackgourndColor(color){
-    document.querySelector('body').style.backgroundColor = color;
+        // document.querySelector('body').style.backgroundColor = color;
+        $('body').css('backgroundColor', color);
     }
 }
 
 function nightDayHandler(self) {
     var target = document.querySelector('body');
-    if(this.value === 'Night'){
-        this.value = 'Day'
+    if(self.value === 'Night'){
         Body.SetBackgourndColor('black');
         Body.SetColor('white');
         Links.SetColor('#FFC107');
+        self.value = 'Day'
         
     } else{
-        this.value = 'Night'
         Body.SetBackgourndColor('white');
         Body.SetColor('black');
         Links.SetColor('#007BFF');
+        self.value = 'Night'
     }
 }
