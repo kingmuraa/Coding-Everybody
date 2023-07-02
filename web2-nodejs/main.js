72e868016ac1db24b71d80794da1b4330744e710
Hyunseok Kim <75778561+kingmuraa@users.noreply.github.com>
2023-07-02 14:10:24 +0900

    글 삭제 기능 완성

--- web2-nodejs/main.js
+++ web2-nodejs/main.js
@@ -63,7 +63,11 @@ var app = http.createServer(function (request, response) {
                         title,
                         list,
                         `<h2>${title}</h2>${description}`,
-                        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+                        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>
+                        <form action="delete_process" method="post">
+                            <input type="hidden" name="id" value="${title}">
+                            <input type="submit" value="delete">
+                        </form>`
                     )
                     response.writeHead(200);
                     response.end(template);
@@ -124,11 +128,37 @@ var app = http.createServer(function (request, response) {
                 response.end(template);
             });
         });
-    // } else if(pathname === '/update_process'){
-
-    }
-    
-    else {
+    } else if (pathname === '/update_process') {
+        var body = '';
+        request.on('data', function (data) {
+            body = body + data;
+        });
+        request.on('end', function () {
+            var post = qs.parse(body);
+            var id = post.id
+            var title = post.title;
+            var description = post.description;
+            fs.rename(`data/${id}`, `data/${title}`, function (err) {
+                fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+                    response.writeHead(302, {Location: `/?id=${title}`});     response.end('');
+                })
+            })
+            console.log(post)
+        });
+    } else if (pathname === '/delete_process') {
+        var body = '';
+        request.on('data', function (data) {
+            body = body + data;
+        });
+        request.on('end', function () {
+            var post = qs.parse(body);
+            var id = post.id
+            fs.unlink(`data/${id}`, function (err) {
+                response.writeHead(302, {Location: `/`});
+                response.end('');
+            });
+        });
+    } else {
         response.writeHead(404);
         response.end('Not Found');
     }
