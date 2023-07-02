31542bad833ffb7ee05c97fea4d288741f08ec84
Hyunseok Kim <75778561+kingmuraa@users.noreply.github.com>
2023-07-02 14:44:12 +0900

    템플릿 모듈화

--- web2-nodejs/main.js
+++ web2-nodejs/main.js
@@ -3,34 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 const {title} = require('process');
-
-template = {
-    HTML: function (title, list, body, control) {
-        return `
-        <!doctype html>
-        <html>
-        <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-        </head>
-        <body>
-        <h1><a href="/">WEB</a></h1>
-        ${list}
-        ${control}
-        ${body}
-        </body>
-        </html>
-        `;
-    },
-    list: function (filelist) {
-        var list = '<ul>';
-        for (i = 0; i < filelist.length; i++) {
-            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-        }
-        list = list + '</ul>';
-        return list
-    }
-}
+var template = require('./lib/template.js');
 
 var app = http.createServer(function (request, response) {
     var _url = request.url;
