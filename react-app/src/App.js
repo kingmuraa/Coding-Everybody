b5ddd29a0af29efd128c311839c12af6ffa5969d
Hyunseok Kim <75778561+kingmuraa@users.noreply.github.com>
2023-07-09 22:38:29 +0900

    react create

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,16 @@
 import './App.css';
 import {useState} from 'react';
 
+
+function Article(props) {
+  return (
+      <article>
+          <h2>{props.title}</h2>
+          {props.body}
+      </article>
+  );
+}
+
 function Header(props) {
     return <h1>
         <a
@@ -35,18 +45,29 @@ function Nav(props) {
     </nav>);
 }
 
-function Article(props) {
-    return (
-        <article>
-            <h2>{props.title}</h2>
-            {props.body}
-        </article>
-    );
+function Create(props) {
+  return (
+    <article>
+      <h2>Create</h2>
+      <form onSubmit={(event)=> { 
+        event.preventDefault();
+        const title = event.target.title.value
+        const body = event.target.body.value
+        props.onCreate(title, body);
+      }}  >
+        <p><input type="text" name="title" placeholder='title'></input></p>
+        <p><textarea name="body" placeholder='body'></textarea></p>
+        <p><input type="submit" value="Create"></input></p>
+      </form>
+    </article>
+  )
 }
+
 function App() {
     const [mode, setMode] = useState('WELCOME');
     const [id, setId] = useState(null);
-    const topics = [
+    const [nextId, setNextId] = useState(4);
+    const [topics, setTopics] = useState([
         {
             id: 1,
             title: 'HTML',
@@ -60,21 +81,31 @@ function App() {
             title: 'JAVASCRIPT',
             body: 'JAVASCRIPT is ...'
         }
-    ]
+    ]);
     let content = null;
     if (mode === 'WELCOME') {
         content = <Article title="WELCOME" body="Hello World"></Article>
     } else if (mode === 'READ') {
-        let title,
-            body = null;
-        for (let i = 0; i < topics.length; i++) {
-            console.log(topics[i].id, id)
-            if (topics[i].id === id) {
-                title = topics[i].title;
-                body = topics[i].body;
-            }
-        }
-        content = <Article title={title} body={body}></Article>;
+      let title,
+          body = null;
+      for (let i = 0; i < topics.length; i++) {
+          console.log(topics[i].id, id)
+          if (topics[i].id === id) {
+              title = topics[i].title;
+              body = topics[i].body;
+          }
+      }
+      content = <Article title={title} body={body}></Article>;
+    } else if(mode === 'CREATE') {
+      content = <Create onCreate={(_title, _body)=>{
+        const newTopic = {id: nextId, title: _title, body: _body};
+        const newTopics = [...topics];
+        newTopics.push(newTopic);
+        setTopics(newTopics);
+        setMode('READ');
+        setId(nextId);
+        setNextId(nextId+1);
+      }}></Create>
     }
     return (
         <div>
@@ -90,6 +121,10 @@ function App() {
                     setId(_id);
                 }}></Nav>
             {content}
+            <a href="/create" onClick={(event) => {
+              event.preventDefault();
+              setMode('CREATE');
+            }}>create</a>
         </div>
     );
 }
