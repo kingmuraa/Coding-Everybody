9f6cb9e6c845bd31ea4324157181542bcaf9c662
Hyunseok Kim <kingmuraa@naver.com>
2023-08-13 15:57:39 +0900

    15. 글수정

--- next-app/src/app/read/[id]/page.js
+++ next-app/src/app/read/[id]/page.js
@@ -1,5 +1,5 @@
 export default async function Read(props) {
-    const resp = await fetch(`http://localhost:9999/topics/${props.params.id}`);
+    const resp = await fetch(`http://localhost:9999/topics/${props.params.id}`, {cache: 'no-cache'});
     const topic = await resp.json();
     return (
         <>
